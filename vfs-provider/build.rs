a797b9b88734f0c291de68fad20394c26e389728
ternbusty <a.iwasaki0128@gmail.com>
2025-11-26 18:53:38 +0900

    chore: remove unused files

--- vfs-provider/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // Generate WIT bindings
-    // For now, we'll use a simpler approach without full WASI dependencies
-    // This is a prototype implementation
-
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=../wit");
-}
